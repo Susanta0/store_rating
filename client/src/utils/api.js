82f66bf5100d94f7bb851b443e555ddb2c808c78
Susanta0 <susanta721467@gmail.com>
2025-05-15 14:21:19 +0530

    deployment server and client

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,22 +1,17 @@
 import axios from "axios"
 
 const isProduction = window.location.hostname !== "localhost";
-const baseUrl= isProduction? "https://store-rating-n3o2.onrender.com":"http://localhost:8000"
+const baseURL= isProduction? "https://store-rating-n3o2.onrender.com":"http://localhost:8000"
 console.log(window.location.hostname);
-console.log(baseUrl);
-
-const api= axios.create({
-    baseURL: baseUrl,
-    withCredentials:false
-})
-
-// Add a request interceptor to always use the latest token
-api.interceptors.request.use(config => {
-    const token = localStorage.getItem("token");
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
+console.log(baseURL);
+const token = localStorage.getItem("token");
+const api = axios.create({
+    baseURL,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+    withCredentials: true,
+  });
+  
 
 export default api
